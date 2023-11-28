32e89918be367f1e0a2c25be96b468e1e5a14bee
Hongri Ma <mahongri1993@gmail.com>
2023-11-28 16:30:48 -0500

    user validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :cars
   has_many :bookings_as_owner, through: :cars, source: :bookings
+  validates :first_name, :last_name, :username, :email_address, presence: true
+  validates :username, uniqueness: true
+  validates :email_address, uniqueness: true
+  validates :username, length: { minimum: 4 }
 end
