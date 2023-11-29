dcf681a17c02486d34c236f0318766c29753af24
Lucie Plantier <lucie.plantier@live.fr>
2023-11-29 16:18:29 -0500

    Changes to the booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:edit, :update]
   def new
     @booking = Booking.new
   end
@@ -16,9 +17,29 @@ class BookingsController < ApplicationController
     end
   end
 
+  def edit
+    validate_owner!
+  end
+
+  def update
+    validate_owner!
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
   private
 
   def booking_params
     params.require(:booking).permit(:starting_date, :ending_date)
+  end
+
+  def validate_owner!
+    if @booking.car.user != current_user
+      redirect_to cars_path, alert: "You're not the owner of the car."
+    end
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
