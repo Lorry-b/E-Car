d055b7e43360c21b5a69e52cc4aa82412b939990
Hongri Ma <mahongri1993@gmail.com>
2023-11-29 12:31:54 -0500

    car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,9 @@
 class Car < ApplicationRecord
+  CATEGORIES = ["SUV", "Pick-up", "Sport Car", "Sedan", "Hatchback"]
   belongs_to :user
   has_many :bookings
   validates :location, presence: true
   validates :start_availability, :end_availability, presence: true
   validates :brand, :category, presence: true
-  validates :category, inclusion: { in: ["SUV", "Pick-up", "Sport Car", "Sedan", "Hatchback"] }
+  validates :category, inclusion: { in: CATEGORIES }
 end
